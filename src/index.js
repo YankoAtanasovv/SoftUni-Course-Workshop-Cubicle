365fe4620b9602a0c95ee3a915609b6019f2b0b9
YankoAtanasovv <btrouble39@gmail.com>
2023-09-07 19:30:30 +0300

    Add cube controller

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 
 const app = express();
@@ -14,6 +15,7 @@ expressConfig(app);
 handlebarsConfig(app);
 
 app.use(homeController);
+app.use('/cubes', cubeController);
 
 
 
