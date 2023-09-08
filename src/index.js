fd4ef9451f48d48df447c76afc6cecabd1431272
YankoAtanasovv <btrouble39@gmail.com>
2023-09-08 18:59:55 +0300

    Add bug fixed with starting the local server

--- src/index.js
+++ src/index.js
@@ -14,5 +14,3 @@ handlebarsConfig(app);
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
-
-app.listen(PORT, () => console.log(`Server is running on ${PORT}...`));
