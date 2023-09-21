b98011ddf5be415e34276e84ed14c99e0bfdbc3a
YankoAtanasovv <btrouble39@gmail.com>
2023-09-21 20:08:32 +0300

    Added new VIEWS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const expressConfig = require("./config/expressConfig");
 const handlebarsConfig = require("./config/handlebarsConfig");
-const dbConnect = require('./config/dbConfig');
+const errorHandler = require("./middlewares/errorHandlerMiddleware");
+const dbConnect = require("./config/dbConfig");
 const routes = require("./routes");
 
 const app = express();
@@ -13,9 +14,10 @@ expressConfig(app);
 handlebarsConfig(app);
 
 dbConnect()
-    .then(() => console.log('DB connected successfully!'))
-    .catch(err => console.log('DB error: ', err.message))
+  .then(() => console.log("DB Connected successfully"))
+  .catch((err) => console.log("DB error: ", err.message));
 
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
