280a744f598427f561f66ad1d3c67bdae2139b57
YankoAtanasovv <btrouble39@gmail.com>
2023-09-21 18:53:36 +0300

    Added userManager

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,52 +1,45 @@
-const uniqid = require('uniqid');
-const cubes = [
-  {
-    id: "1n73sh8holhz66elc",
-    name: "Mirror Cube",
-    description: "A cool mirror cube",
-    imageUrl: "https://m.media-amazon.com/images/I/71TrvUl50OL.jpg",
-    difficultyLevel: 4,
-  },
-  {
-    id: "2n73sh8holaz66elc",
-    name: "Rubic Classic",
-    description: "Evergreen",
-    imageUrl:
-      "https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067-675x380.jpg",
-    difficultyLevel: 3,
-  },
-];
+const Cube = require("../models/Cube");
 
-exports.getAll = (search, from, to) => {
-    let result = cubes.slice();
+exports.getAll = async (search, from, to) => {
+  let result = await Cube.find().lean();
 
-    if (search) {
-        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+  // TODO: use mongoose to filter in the db
+  if (search) {
+    result = result.filter((cube) =>
+      cube.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }
 
-    }
+  if (from) {
+    result = result.filter((cube) => cube.difficultyLevel >= Number(from));
+  }
 
-    if (from) {
-        result = result.filter(cube => cube.difficultyLevel >= Number(from));
+  if (to) {
+    result = result.filter((cube) => cube.difficultyLevel <= Number(to));
+  }
 
-    }
+  return result;
+};
 
-    if (to) {
-        result = result.filter(cube => cube.difficultyLevel <= Number(to));
-    }
-
-    return result;
-
-
-}
-
-exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId);
+exports.getOneWithAccessories = (cubeId) =>
+  this.getOne(cubeId).populate("accessories");
 
 exports.create = (cubeData) => {
-    const newCube = {
-        id: uniqid(),
-        ...cubeData,
-    };
-    cubes.push(newCube);
+  const cube = new Cube(cubeData);
 
-    return newCube;
-}
+  return cube.save();
+};
+
+exports.update = (cubeId, cubeData) => Cube.findByIdAndUpdate(cubeId, cubeData);
+
+exports.delete = (cubeId) => Cube.findByIdAndDelete(cubeId);
+
+exports.attachAccessory = async (cubeId, accessoryId) => {
+  // return Cube.findByIdAndUpdate(cubeId, { $push: { accessories: accessoryId } });
+
+  const cube = await Cube.findById(cubeId);
+  cube.accessories.push(accessoryId);
+
+  return cube.save();
+};
